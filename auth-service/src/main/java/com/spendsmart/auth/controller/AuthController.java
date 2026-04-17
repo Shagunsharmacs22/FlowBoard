10421b1ec40b465b33bfe9d0b68a60050e590866
shagunsharmacs22 <shagunmanishsharma@gmail.com>
2026-04-17 15:07:23 +0530

    [Shagun].fix some files

--- auth-service/src/main/java/com/spendsmart/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/spendsmart/auth/controller/AuthController.java
@@ -28,7 +28,7 @@ import java.util.Map;
  *   GET    /auth/users            (admin helper)
  */
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
